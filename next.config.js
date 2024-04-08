94bf5556eb4a46dada1ef25959460ef068fc5060
Yogesh Shahi <65803503+BornPsych@users.noreply.github.com>
2024-04-08 13:59:01 +0530

    chore: updated bundle Analyser and postcss config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
+//eslint-ignore
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withBundleAnalyzer({ enabled: true })(nextConfig);
+export default withBundleAnalyzer(nextConfig);
