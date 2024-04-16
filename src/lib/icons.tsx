e4f53a086b7bb59022f11327532973a259831451
Yogesh Shahi <65803503+BornPsych@users.noreply.github.com>
2024-04-16 22:47:35 +0530

    feat: icons added on teh client side

---
+++ src/lib/icons.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  ArrowUpRight,
+  DiscordLogo,
+  XLogo,
+  YoutubeLogo,
+} from '@phosphor-icons/react';
+
+export { ArrowUpRight, DiscordLogo, XLogo, YoutubeLogo };
