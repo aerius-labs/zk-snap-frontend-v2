0d01a652626f206013b919f4e1f3bb5cbb7611d7
Manav Babber <manavfazilka@gmail.com>
2024-04-16 14:15:52 +0530

    chore: removed faq from root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-12 md:p-24'>
       <h1>ZK Snap</h1>
-      <FAQ />
     </main>
   );
 }
