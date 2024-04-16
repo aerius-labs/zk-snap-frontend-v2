ee787493b3373e87e960983275a1b554113ff1cb
Yogesh Shahi <65803503+BornPsych@users.noreply.github.com>
2024-04-17 01:56:32 +0530

    chore: added icons for sidebar

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -3,9 +3,22 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {
   ArrowUpRight,
+  Bell,
   DiscordLogo,
+  File,
+  House,
+  UsersThree,
   XLogo,
   YoutubeLogo,
 } from '@phosphor-icons/react';
 
-export { ArrowUpRight, DiscordLogo, XLogo, YoutubeLogo };
+export {
+  ArrowUpRight,
+  Bell,
+  DiscordLogo,
+  File,
+  House,
+  UsersThree,
+  XLogo,
+  YoutubeLogo,
+};
