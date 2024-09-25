652e35625467618a86955065182465b7d756bfbf
manavbabber <manavfazilka@gmail.com>
2024-09-25 18:58:43 +0530

    feat: added required shadcn components

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+import type { ClassValue } from 'clsx'; // Correct import for TypeScript types
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
