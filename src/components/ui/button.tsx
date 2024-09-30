aa3b62a9f55fd0d6cc14674fceac174695a0cd04
manavbabber <manavfazilka@gmail.com>
2024-09-30 12:35:26 +0530

    feat: doc on mobile and tab instead of sidebar

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,8 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost:
+          'hover:bg-purple-400 hover:text-gray-300 text-gray-200 bg-purple-300',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
